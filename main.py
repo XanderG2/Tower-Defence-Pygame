1c66985fd2fa47bee74ac005c1624d552bcc0b9e
XanderG2 <XanderG2@users.noreply.github.com>
2023-11-19 14:05:40 +0000

    add colours

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ height = 500
 screen = pygame.display.set_mode((width, height))
 
 white = (255,255,255)
+black = (0,0,0)
+lightGreen = (0,255,0)
+darkLightGreen = (51,153,51)
+midGreen = (0,204,0)
+darkGreen = (0,153,51)
+veryDarkGreen = (0,102,0)
 
 running = True
 while running:

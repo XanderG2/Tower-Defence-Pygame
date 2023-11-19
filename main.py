483cf207b951e51bee3c93fc1c5ef1fb1e628fde
XanderG2 <XanderG2@users.noreply.github.com>
2023-11-19 14:18:23 +0000

    grid

--- main.py
+++ main.py
@@ -13,13 +13,34 @@ midGreen = (0,204,0)
 darkGreen = (0,153,51)
 veryDarkGreen = (0,102,0)
 
+cellSize = 20
+gridCoords = []
+
+for x in range(0, width, cellSize):
+    for y in range(0, height, cellSize):
+        top_right = (x + cellSize, y)
+        bottom_left = (x, y + cellSize)
+        gridCoords.append([top_right, bottom_left])
+
+print(gridCoords)
+
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             sys.exit()
     screen.fill(white)
+    for x in range(0, width, cellSize):
+        pygame.draw.line(screen, black, (x, 0), (x, height))
+    for y in range(0, height, cellSize):
+        pygame.draw.line(screen, black, (0, y), (width, y))
+    mousePos = pygame.mouse.get_pos()
+    for index, coords in enumerate(gridCoords):
+        top_right, bottom_left = coords
+        if bottom_left[0] <= mousePos[0] <= top_right[0] and bottom_left[1] <= mousePos[1] <= top_right[1]:
+            print(f"Mouse is in cell {index}")
     pygame.display.flip()
 
 pygame.quit()
